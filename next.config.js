f36e0d345dba744531bcab3422dd969ac3e810df
David Vaverka <d.vaverka@gmail.com>
2023-04-25 16:21:36 +0200

    format code

--- next.config.js
+++ next.config.js
@@ -9,6 +9,19 @@ const nextConfig = {
     config.watchOptions.ignored = /node_modules/
     return config
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=9999999999, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
